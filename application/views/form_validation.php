211840119679794e20939e49ee4f4efd82ea5247
shanmugapriyasivaraj <mpriyasivaraj28@gmail.com>
2022-06-17 11:27:32 +0530

    first commit

---
+++ application/views/form_validation.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+
+    <title>Form Validation</title>
+    <script>
+        src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"
+    </script>
+    <link rel="stylesheet" href="<link rel = "stylesheet" href = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
+    <script src =  "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>    
+    </head>
+    <body>
+        <div class="container">
+            <br />
+            <h3 align="center">Form Validation</h3>
+        </div>
+    </body>
+</html>
